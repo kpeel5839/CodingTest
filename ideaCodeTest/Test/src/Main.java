83f9cf5a6be1f64d13cee76b54f2df210e87867b
jaeyeon kim <jakind@naver.com>
2021-12-06 12:01:30 +0900

    5373 문제 해결

--- ideaCodeTest/Test/src/Main.java
+++ ideaCodeTest/Test/src/Main.java
@@ -2,35 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Main{
     public static void main(String[] args){
-        int n = 3;
-        int[][] map = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                map[i][j] = j;
-            }
-        }
-
-        System.out.println("original break test");
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){ //그냥 일반적인 break 문 test
-                System.out.print(map[i][j] + " ");
-                if(map[i][j] == 1){
-                    break;
-                }
-            }
-            System.out.println();
-        }
-
-        System.out.println("loop break test ");
-        Loop1:
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                System.out.print(map[i][j] + " " );
-                if(map[i][j] == 1){
-                    break Loop1;
-                }
-            }
-            System.out.println();
-        }
+        Object[] list = new Object[]{2 , "hello", -3.4};
+        System.out.println(Arrays.toString(list));
     }
 }
