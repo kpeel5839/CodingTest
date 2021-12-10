53bc5336a1c88f6d7c0cf06214a90d673ad0a3c4
jaeyeon kim <jakind@naver.com>
2021-12-10 13:12:20 +0900

    17143 문제 해결

--- ideaCodeTest/17143_problem/src/Main.java
+++ ideaCodeTest/17143_problem/src/Main.java
@@ -2,8 +2,9 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static int r , c , sharkCount , total = 0 , globalDir;
-    public static Shark[][] sharks; //shark의 정보를 배열에다가 집어넣자.
+    public static Shark[][] sharks;
     public static int[] dx = {0 , 0 , 1 , -1} , dy = {-1 , 1 , 0 , 0}; //0 위 , 1 아래 , 2 오른쪽 , 3 왼쪽
+    public static Queue<Shark> moveBefore = new LinkedList<>() , moveAfter = new LinkedList<>();
     //(i + 1 == 2 ? 0 : i + 1 == 4 ? 2 : i + 1) -- 반대 방향 계산하는 방법
     public static void main(String[] args) throws IOException{ //샤크의 정보를 가지고 계속 맵에다가 그려넣는다 , 그려 넣고서 상어의 정보를 다시 받는 거지
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -17,34 +18,17 @@ public class Main {
 
         for(int i = 0; i < sharkCount; i++){
             st = new StringTokenizer(input.readLine());
-            sharks[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = new Shark(Integer.parseInt(st.nextToken()) ,Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()));
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int speed = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken()) - 1;
+            int size = Integer.parseInt(st.nextToken());
+            sharks[y][x] = (new Shark(y , x , speed , dir , size));
         }
         //낚시왕이 잡고서 움직여야함 ,
         for(int i = 0; i < c; i++){
             fishing(i);
-            System.out.println("fishing after");
-            for(int j = 0; j < r; j++){
-                for(int v = 0; v < c; v++){
-                    if(sharks[j][v] == null){
-                        System.out.print(0 + " ");
-                        continue;
-                    }
-                    System.out.print(1 + " ");
-                }
-                System.out.println();
-            }
             instructMove();
-            System.out.println("move after");
-            for(int j = 0; j < r; j++){
-                for(int v = 0; v < c; v++){
-                    if(sharks[j][v] == null){
-                        System.out.print(0 + " ");
-                        continue;
-                    }
-                    System.out.print(1 + " ");
-                }
-                System.out.println();
-            }
         }
         System.out.println(total);
     }
@@ -57,84 +41,93 @@ public class Main {
             }
         }
     }
-    public static void instructMove(){
+    public static void instructMove(){//shark 하나씩 돌면서 다 move 시키기 그러면서 list에다가 쌓기 그 다음에 list하나씩 다 돌면서 collection.sort한 뒤에 제일 최상위에 있는놈만 남기기
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
-                if(sharks[i][j] != null){ //여기서부터 move 들어간다
-                    Point point = move(i , j);
-                    Shark shark = sharks[i][j];
-                    shark.dir = globalDir;
+                if(sharks[i][j] != null){
+                    moveBefore.add(sharks[i][j]);
                     sharks[i][j] = null;
-                    if(sharks[point.y][point.x] != null){
-                        if(shark.size > sharks[point.y][point.x].size){
-                            sharks[point.y][point.x] = shark;
-                        }
-                    }
-                    else{
-                        sharks[point.y][point.x] = shark;
-                    }
                 }
             }
         }
+        while(!moveBefore.isEmpty()){
+            Shark shark = moveBefore.poll();
+            Point point = move(shark);
+            shark.dir = globalDir;
+            shark.y = point.y;
+            shark.x = point.x;
+            moveAfter.add(shark);
+        }
+        while(!moveAfter.isEmpty()){
+            Shark shark = moveAfter.poll();
+            if(sharks[shark.y][shark.x] != null){
+                if(shark.size > sharks[shark.y][shark.x].size){
+                    sharks[shark.y][shark.x] = shark;
+                }
+            }
+            else{
+                sharks[shark.y][shark.x] = shark;
+            }
+        }
     }
-    public static Point move(int row , int col){
-        Shark shark = sharks[row][col];
+    public static Point move(Shark shark){
+        int row = shark.y;
+        int col = shark.x;
         int speed = shark.speed;
-        int dir = shark.dir;
-//        if(dir == 2 || dir == 3){ //x축에 관해서 연산
-//            int end = dir == 2 ? c - 1 : 0;
-//            if(speed < Math.abs(end - col)){
-//                globalDir = dir;
-//                return new Point(row , col + speed * dx[dir]);
-//            }
-//            speed -= Math.abs(end - col);
-//            int start;
-//            boolean reverse = false;
-//            if(speed / (c - 1) % 2 == 0){ //짝수면 같은 방향 x
-//                start = (end == c - 1) ? c - 1 : 0;
-//                reverse = true;
-//            }
-//            else{
-//                start = (end == c - 1) ? 0 : c - 1;
-//            }
-//            int dis = speed % (c - 1);
-//            globalDir = reverse == false ? dir : dir + 1 == 2 ? 0 : dir + 1 == 4 ? 2 : dir + 1;
-//            return new Point(row , start + (dis * dx[globalDir]));
-//        }
-//        else{ //y축에 관해서 연산
-//            int end = dir == 1 ? r - 1 : 0;
-//            if(speed < Math.abs(end - row)){
-//                globalDir = dir;
-//                return new Point(row + speed * dy[dir] , col);
-//            }
-//            speed -= Math.abs(end - col);
-//            int start;
-//            boolean reverse = false;
-//            if(speed / (r - 1) % 2 == 0){ //짝수면 같은 방향 x
-//                start = (end == r - 1) ? r - 1 : 0;
-//                reverse = true;
-//            }
-//            else{
-//                start = (end == r - 1) ? 0 : r - 1;
-//            }
-//            int dis = speed % (r - 1);
-//            globalDir = reverse == false ? dir : dir + 1 == 2 ? 0 : dir + 1 == 4 ? 2 : dir + 1;
-//            return new Point(start + (dis * dy[globalDir]) , col);
-//        }
+        int dir = shark.dir; // 2  , 3 오른쪽(1) 왼쪽(-1) 0 , 1 위쪽(-1) 아랫쪽(1)
+        if(dir == 2 || dir == 3){
+            int end = dir == 2 ? c - 1 : 0;
+            if(speed < Math.abs(end - col)){
+                globalDir = dir; //dir 유지
+                return new Point(row , col + (speed * dx[dir]));
+            }
+            speed -= Math.abs(end - col);
+            int start;
+            boolean reverse = false;
+            if((speed / (c - 1)) % 2 == 0){
+                start = (end == (c - 1)) ? c - 1 : 0;
+                reverse = true;
+            }
+            else{
+                start = (end == (c - 1)) ? 0 : c - 1;
+            }
+            int dis = speed % (c - 1);
+            globalDir = (reverse == false) ? dir : (dir + 1 == 2 ? 0 : dir + 1 == 4 ? 2 : dir + 1);
+            return new Point(row , start + (dis * dx[globalDir]));
+        }
+        else{
+            int end = dir == 1 ? r - 1 : 0;
+            if(speed < Math.abs(end - row)){
+                globalDir = dir; //dir 유지
+                return new Point(row + (speed * dy[dir]) , col);
+            }
+            speed -= Math.abs(end - row);
+            int start;
+            boolean reverse = false;
+            if((speed / (r - 1)) % 2 == 0){
+                start = (end == (r - 1)) ? r - 1 : 0;
+                reverse = true;
+            }
+            else{
+                start = (end == (r - 1)) ? 0 : r - 1;
+            }
+            int dis = speed % (r - 1);
+            globalDir = (reverse == false) ? dir : (dir + 1 == 2 ? 0 : dir + 1 == 4 ? 2 : dir + 1);
+            return new Point(start + (dis * dy[globalDir]) , col);
+        }
     }
-
     public static class Shark{
         int speed;
         int dir;
         int size;
-        public Shark(int speed , int dir , int size){
+        int y;
+        int x;
+        public Shark(int y , int x, int speed , int dir , int size){
             this.speed = speed;
             this.dir = dir;
             this.size = size;
-        }
-        @Override
-        public String toString(){
-            return "speed : " + speed + " dir : " + dir + " size : " + size;
+            this.y = y;
+            this.x = x;
         }
     }
     public static class Point{
@@ -145,5 +138,4 @@ public class Main {
             this.x = x;
         }
     }
-
 }
--- ideaCodeTest/Test/src/Main.java
+++ ideaCodeTest/Test/src/Main.java
@@ -1,10 +1,19 @@
 import java.util.*;
 import java.io.*;
 public class Main{
+    public static List<Integer> number = new ArrayList<>();
     public static void main(String[] args){
-        for(int i = 0; i < 4; i++){
-            System.out.println("now index : " + i);
-            System.out.println("reverse index : " + (i + 1 == 2 ? 0 : i + 1 == 4 ? 2 : i + 1));
+        for(int i = 4; i != -1; i--){
+            number.add(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
+        Collections.sort(number);
+        for(int num : number){
+            System.out.print(num + " " );
+        }
+        System.out.println();
+
+        System.out.println(number.get(number.size() - 1));
     }
 }
