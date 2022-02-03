d90170636e21da97a24aa10ed027bc3bcd99834a
jaeyeon kim <jakind@naver.com>
2022-02-03 20:54:07 +0900

    1086번 문제 해결

--- ideaCodeTest/Test/src/Main.java
+++ ideaCodeTest/Test/src/Main.java
@@ -2,18 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    public static int gcd(int a , int b){
+        /*
+        gcd를 재귀 호출 해서 b를 0으로 만들면 a가 최대공약수이니 그 때 반환하면된다.
+        근데 만약에 b가 0이 안된다? 그러면 최대 공약수가 없는 것이다.
+         */
+        System.out.println("a : " + a + " b: " + b);
+        if(b == 0){
+            return a;
+        }
+        else{
+            return gcd(b , a % b);
+        }
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(input.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        queue.add(4);
-        System.out.println(queue.size());
-        while(!queue.isEmpty()){
-            System.out.println(queue.poll());
-        }
+        int number1 = Integer.parseInt(st.nextToken());
+        int number2 = Integer.parseInt(st.nextToken());
+        System.out.println(gcd(Math.max(number1 , number2) , Math.min(number1 , number2)));
     }
 }
