08d6830e991ef4f9d68bd327084fea62a6974915
jaeyeon kim <jakind@naver.com>
2022-02-20 13:56:56 +0900

    문제 추가

---
+++ ideaCodeTest/14938_problem/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+// 14938 : 서강그라운드
+/*
+-- 전체 설계
+
+-- 틀 설계
+
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
---
+++ ideaCodeTest/1956_problem/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+// 1956 : 운동
+/*
+-- 전체 설계
+
+-- 틀 설계
+
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
