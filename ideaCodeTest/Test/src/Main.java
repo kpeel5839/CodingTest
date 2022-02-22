21b3c11556fd1a8bd3d625439d6213b678dc24b1
jaeyeon kim <jakind@naver.com>
2022-02-22 23:34:17 +0900

    [#3] Update , Delete 추가 및 기수별 조회 기능 , 정렬 수정

--- ideaCodeTest/Test/src/Main.java
+++ ideaCodeTest/Test/src/Main.java
@@ -1,19 +1,62 @@
-import java.awt.*;
 import java.util.*;
 import java.io.*;
-import java.util.List;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(input.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
+        int n = Integer.parseInt(input.readLine());
+        int[] numberList = new int[n];
+        int[] countList = new int[8001]; // 0 == -4000 , 4000 = 0 , 8000 = 4000
+        double sum = 0;
+        int modeResult = 0;
 
-        System.out.println(queue.peek());
-        System.out.println(queue.poll());
+        for(int i = 0; i < n; i++){
+            int number = Integer.parseInt(input.readLine());
+            countList[number + 4000]++;
+            numberList[i] = number;
+            sum += number;
+        } // 범위 , 가장 큰 값 - 가장 작은 값 + 1 본인도 포함이라서
+        // 중앙 값 정렬후 중간값 , 무조건 n 홀수
+        // 최빈 값은 countList에서 가장 높은 숫자 선택
+        // 산술평균 sum / n 하고 반올림
+
+        int maxMode = 0;
+        int modeIndex = 0;
+        int modeCount = 1;
+        Arrays.sort(numberList);
+
+        for(int i = 0; i <= 8000; i++){
+            if(maxMode < countList[i]) {
+                maxMode = countList[i];
+                modeIndex = i;
+                modeCount = 1;
+            }
+            else if(maxMode == countList[i]){
+                modeCount++;
+            }
+        }
+
+        if(modeCount == 1){
+            modeResult = modeIndex;
+        }else{
+            modeCount = 0;
+            for(int i = 0; i <= 8000; i++){
+                if(maxMode == countList[i]){
+                    modeCount++;
+                    if(modeCount == 2){
+                        modeResult = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        int range = numberList[numberList.length - 1] - numberList[0];
+
+        System.out.println(Math.round(sum / n));
+        System.out.println(numberList[numberList.length / 2]);
+        System.out.println(modeResult - 4000);
+        System.out.println(range);
     }
 }
