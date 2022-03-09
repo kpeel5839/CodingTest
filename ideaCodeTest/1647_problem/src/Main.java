6f489a3c51791b082970b43da865aa264b58fd5d
jaeyeon kim <jakind@naver.com>
2022-03-09 17:01:45 +0900

    1647번 문제 해결

--- ideaCodeTest/1647_problem/src/Main.java
+++ ideaCodeTest/1647_problem/src/Main.java
@@ -5,12 +5,110 @@ import java.io.*;
 
 /*
 -- 전제 조건
+도시를 두개의 마을로 분할하는데 , 각 안의 마을들의 경로가 항상 존재하고 ,
+모든 엣지들의 유지비를 최소로 하고 싶다.
 
+간단하다 , 최소신장트리 알고리즘을 사용하면서 , 간선의 개수는 항상 n - 1 개로 유지해야한다.
+근데 , 마을을 2개로 분할하고 , 각 마을들의 경로를 다 이어지게 만들라는 것은 간선을 최소신장트리에 맞게 , n - 2 개를 골랐을 때의 거리의 합을 구하면 된다.
 -- 틀 설계
+union , find 로 N + 1 크기만한 parent 를 만들고 , 다 부모를 본인으로 한다.
+그 다음에 union 은 각 집합을 합쳐주고 , find 는 해당 엣지에 이어진 정점 두개의 부모를 찾아주는 역할을 한다.
+
+그리고 n - 2 개를 선택할 때 까지 가면서 , 선택한 cost 들의 합을 출력하면 될 것 같다.
+
+-- 해맸던 점
+이상하다 , 항상 comparable 객체로 정렬을 진행했었는데 , 자꾸 illegalArgument exception 이 나서
+그냥 람다식으로 정렬을 해주었더니 해결이 되었다.
  */
 public class Main {
+    public static int[] parent;
+    public static int V , E , ans = 0;
+    public static List<Edge> edgeList = new ArrayList<>();
+    public static int find(int number){
+        /*
+        parent 를 찾아주면서 ,
+        parent 를 찾으면 모두 parent를 최상위 부모로 바꾼다.
+         */
+        if(number == parent[number]){
+            return number;
+        }
+        return parent[number] = find(parent[number]); // 일단 부모를 찾으러 현재 자신의 부모로 점프하고 , find로 인해서 , 최종적으로 반환되는 부모값을 본인의 parent 값으로 바꾼다.
+    }
+    public static void union(int a, int b){
+        /*
+        a , b 의 부모를 얻고,
+        parent[b] = a 를 넣어서
+        두개의 집합을 합친다.
+         */
+
+        a = find(a);
+        b = find(b);
+
+        parent[b] = a; // a의 부모를 b의 부모의 부모로 지정한다. 그렇게 되면서 두개의 집합이 같아지게 된다.
+    }
+    public static class Edge{
+        int v1;
+        int v2;
+        int cost;
+        public Edge(int v1 , int v2 , int cost){
+            this.v1 = v1;
+            this.v2 = v2;
+            this.cost = cost;
+        }
+//
+//        @Override
+//        public int compareTo(Edge o){
+//            if(this.cost >= o.cost){
+//                return 1;
+//            }
+//            else {
+//                return -1;
+//            }
+//        }
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(input.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        parent = new int[V + 1];
+
+        for(int i = 0; i <= V; i++){
+            parent[i] = i;
+        }
+
+        for(int i = 0; i < E; i++){
+            st = new StringTokenizer(input.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            edgeList.add(new Edge(v1 , v2 , cost)); // 무방향 그래프
+        }
+
+        Collections.sort(edgeList, (o1 , o2) -> Integer.compare(o1.cost , o2.cost)); // 정렬 정상적으로 완료
+
+//        for(Edge edge : edgeList){
+//            System.out.println(edge.cost);
+//        }
+
+        int count = 0;
+        for(Edge edge : edgeList){
+            if(count == V - 2) break;
+
+            int a = find(edge.v1);
+            int b = find(edge.v2); // 두개의 부모가 같으면 진행하면 안됨
+
+            if(a == b) continue;
+
+            // 여기까지 내려온 건 아니라는 것을 의미함
+            union(a , b);
+            ans += edge.cost;
+            count++;
+        }
+
+        System.out.println(ans);
     }
 }
--- ideaCodeTest/6064_problem/src/Main.java
+++ ideaCodeTest/6064_problem/src/Main.java
@@ -1,100 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-// 6047 : 카잉 달력
+// 6064 : 카잉 달력
 
 /*
 -- 전제 조건
-N개의 에너지 구슬이 일렬로 놓여져 있고 , 에너지 구슬을 이용해서 에너지를 모으려고 한다.
-i 번째 에너지 구슬의 무게는 Wi 이고 , 에너지를 모으는 방법은 다음과 같다.
-
-1. 에너지 구슬을 하나를 고른다. 고른 에너지 구슬의 번호를 x라고 한다. (고를 때 첫번째 마지막 에너지 구슬은 고를 수 없음)
-2. x번째 에너지 구슬을 제거한다.
-3. W(x - 1) * W(x + 1) 즉 , 주변에 있는 에너지 구슬의 곱만 큼 모을 수 있음 , 그래서 첫번째이랑 마지막 구슬은 고를 수 없었던 듯
-4. N을 1을 감소시키고 , 에너지 구슬을 1번부터 N번까지로 다시 번호를 매긴다 , 번호는 첫 구슬이 1번 ,.... N - 1 이렇게이다.
-
-N과 에너지 구슬의 무게가 주어졌을 때 , 모을 수 있는 에너지 양의 최댓값을 구하는 프로그램을 작성
 
 -- 틀 설계
-4
-1 2 3 4
-
-일 때 , 3을 제거하고 , 2를 제거하면 , 12라는 에너지의 최댓값을 얻어낼 수 있다.
-
-이 문제에서는 그냥 , 첫번째 구슬 , 마지막 구슬을 주의하고 , 이 구슬을 선택할지 안할지를 결정하면 될 것 같다.
-근데 그 행위를 반복해야 한다.
  */
 public class Main {
-    public static int ans = 0 , N;
-    public static int[] marble;
-    public static boolean[] visited;
-    public static void dfs(int sum , int remain){
-        /*
-        dfs 에서 확인해야 할 것 , 이게 양쪽에 구슬이 있는 구슬인지
-        이 구슬이 이미 선택한 구슬인지 아닌지
-        이 구슬을 지금 선택할 것인지 안할 것인지
-
-        양쪽에 구슬이 있는지 알려면 , 이게 몇번째 구슬인지 알아야 하지 않을까?
-        그리고 구슬의 남은 개수가 이제 2개 밖에 없으면 에너지를 ans와 비교해서 넣어야함
-
-        이렇게 한번 해보자.
-        처음에 입력이 들어오면 함수 내부에서 int 배열을 하나 만들어서(짜피 크기 10이라서 오버헤드 별로 없음)
-        순서를 입력해주는 것이다. visited 여부로 따라서 remain 이 2이면 그냥 바로 리턴이라서 상관 x
-
-        그리고서 , 1과 N이 아니면 선택하거나 선택하지 않거나로 하는데 ,
-        선택하면 0번째부터 다시 탐색해야하고
-        선택하지 않으면 그냥 continue를 해준다.
-
-        선택한 구슬은 visited 처리를 한다.
-         */
-
-        if(remain == 2){
-            ans = Math.max(sum , ans);
-//            System.out.println(sum);
-            return;
-        }
-
-        int[] sequence = new int[N];
-        int count = 1;
-
-        for(int i = 0; i < N; i++){
-            if(!visited[i]) sequence[i] = count++; // 선택하지 않았으면 , sequence 에다가 넣어준다.
-        }
-
-        count--;
-
-        for(int i = 0; i < sequence.length; i++){ // sequence length 만큼 진행해준다.
-            if(sequence[i] == 1 || sequence[i] == count) continue;
-            if(sequence[i] == 0) continue;
-            visited[i] = true;
-            dfs(sum + marble[findSequence(sequence , sequence[i] - 1)] * marble[findSequence(sequence , sequence[i] + 1)] , remain - 1); // 선택하지 않는 경우는 굳이 넘기지 않는다.
-            visited[i] = false;
-        }
-    }
-
-    public static int findSequence(int[] sequence , int find){
-        for(int i = 0; i < N; i++){
-            if(sequence[i] == find) return i;
-        }
-        return 0; // 없는 경우 없음
-    }
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        N = Integer.parseInt(input.readLine());
-
-        marble = new int[N];
-        visited = new boolean[N];
-
-        st = new StringTokenizer(input.readLine());
-
-        for(int i = 0; i < N; i++){
-            marble[i] = Integer.parseInt(st.nextToken());
-        }
-
-        dfs(0 , N);
-
-        System.out.println(ans);
     }
 }
