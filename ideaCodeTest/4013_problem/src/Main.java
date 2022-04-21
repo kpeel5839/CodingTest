dcccfa84ca42bb18890a72bd61d1c5a6e6932535
jaeyeon kim <jakind@naver.com>
2022-04-21 11:00:53 +0900

    4013번 문제 해결

--- ideaCodeTest/4013_problem/src/Main.java
+++ ideaCodeTest/4013_problem/src/Main.java
@@ -5,11 +5,215 @@ import java.io.*;
 
 /*
 -- 전제조건
+교차로 들이 존재하고
+거기서 ATM 기는 모든 교차로에 설치되어 있다.
+교차로 끼리 사이클이 형성되어 있고,
+레스토랑이 있는데
+
+아무 레스토랑 점에 들어가고 , 거까지 가는데 ATM 기에서 최대의 돈을 인출할 수 있는 알고리즘을 작성하고 , 그 값을 출력하라.
+
 -- 틀설계
+일단 , 사이클을 파악하는 SCC 집합을 결정해야한다.
+그 다음에 , 그 SCC 들을 가지고 위상정렬을 진행할 것인데 , 여기서 가장 중요한 것은 SCC 의 가중치는 내부 정점들의 가중치의 합이라는 것이다.
+SCC 들은 서로 사이클이 형성될 수 없다. 그것은 이전에 3977 (축구전술) 번을 풀면서 꺠달은 점이다.
+그래서 , 확실하게 서로 사이클이 형성될 수 없다라는 것을 알게 되었으니까 , 이 점을 이용하여서 위상정렬을 진행하게 된다면,
+순서대로 모든 SCC 들을 방문할 수 있다라는 것을 알 수 있다.
+
+여기서 가장 중요한 점은 , 어떠한 지점에 들어섰을 때 , 그 지점이 SCC 집합이라면 무조건적으로 당연하게도 그 사이클 내의 정점들의 가중치값들은 확실하게 얻을 수가 있다.
+그것을 보고 SCC 의 가중치들을 저장해야 한다라는 것을 깨달을 수 있다.
+
+그러면 이제 SCC 들의 순서가 중요한데 , 서로 사이클이 형성이 되어 있지 않으니 , 무조건 적으로 해당 SCC 집합이 실행이 되려면 진입차선이 없어야 한다.
+그러면 만일 진입차선이 원래 존재했더라면? 그 본인의 SCC 로 진입하던 SCC 의 값은 이미 dynamic programming 으로 인해서 값이 결정된 후일 것이고
+그리고 , 본인의 값도 결정되어 있는 다음일 것이다.
+(이전 진입 SCC 들은 서로 사이클이 형성되어 있지 않고 , 그렇기 때문에 서로의! 값에는 영향을 줄 수 없다 이전 진입차선들이 동일한 레벨이라고 했을 때이고 , 그리고 서로! 영향을 줄 수 없다라는 것에 주의)
+
+그러면 그 점을 이용해서 위와 같은 결론을 얻을 수 있고,
+이렇게 정해진 값들과 , 현재 내 SCC 내에 레스토랑이 포함되어 있다라는 것을 인지를 하면??
+Queue 에서 해당 SCC 가 빠졌을 때 , 이 SCC 에 레스토랑이 포함이 되어 있다면 결과값으로 Math.max 하면 되는 것이다.
+
+당연하게도 ATM 에는 음수값은 존재하지 않고 , 50만개에 각각의 ATM 에는 최대 4000의 값이 저장되어 있으니 , 20억이다.
+즉 , 결과값은 overflow 될 수 없으니 , int로 진행해도 된다.
+
+-- 해맸던 점
+또 개같이.. id[next] 값과 min 값을 비교해줬었어야 했는데 , 그렇지 않아서 시간초과 났었음 , 틀렸다 나왔으면 조금 빨리 찾았을 텐데 , 틀렸음 나와가지고
+입력 Stream 으로 바꾸고 생 조랄을 했음
+그 다음에 , 시작정점을 기준으로 위상정렬을 했었어야 했는데 , 그렇지 못했어써
+수정했었음 , 그래서 내 생각으로는 시작정점을 기준으로 위상정렬을 하려면 , 재귀방법으로 밖에 떠오르지 않아서 ,
+해봤는데 시간초과 났음 , 그래서 어쩔 수 없이 결과를 본 결과 , 그냥 다익스트라 형식으로 하는데 , 중간에 끝내고 , 그런 과정이 없어서
+그냥 , queue 로 진행했었음 , 시작정점이 있었기에 다익스트라 처럼 진행하였고 , 위상정렬 개같은 카테고리 때문에 오히려 더 헷갈렸음.
+근데 분명히 위상정렬로 풀 수 있는데 오래걸린다... 내가 분명 재귀적으로 잘 못 구성한 것이긴 할 듯
+그리고 다익스트라를 Queue 로 진행했는데 , 짜피 PriorityQueue 로 진행하는 이유자체가 , cost 높은 걸로 진행하는 건데,
+일단 이거는 모든 정점을 다 갱신하고 거기서 최대값을 찾아내는 것이기 때문에 , 목표 정점이 없다라는 것 , 그리고 queue 같은 경우는 그냥 삽입이 상수시간이지만 PriorityQueue 는 logN 이기 때문에 오히려 시간이 손해일 것이다.
+그리고 , 비교를 통해서 queue 에다가 add 하는 과정을 진행하기에 , 분명히 비효율적인 경우도 존재하겠지만 , PriorityQueue 로 진행할 이유가 없고 , 그리고 무엇보다 PrirorityQueue 는 해당 정점까지의 cost를 기준으로 하기에
+요소도 2개넣어야 한다 , 귀찮다 , 그래서 이런식으로 해결하였음
+
+그래서 결과적으로
+SCC , 다익스트라로 해결한 것이다.
  */
 public class Main {
+//    public static int[] entry;
+    public static int[] id , scc , vertex , dp , sccVertex;
+    public static boolean[] resto , sccResto;
+    public static int res = 0 , V , E , S , idValue = 0 , sccSize = 0;
+    public static Stack<Integer> stack = new Stack<>();
+    public static List<ArrayList<Integer>> graph = new ArrayList<>();
+    public static List<ArrayList<Integer>> sccGraph = new ArrayList<>();
+
+    // 강연결집합 찾기
+    public static int scc(int a){
+        id[a] = ++idValue;
+        stack.push(a);
+
+        int min = id[a];
+        for(Integer next : graph.get(a)){
+            // 다음 정점이 아직 방문치 않았으면
+            if(id[next] == 0) min = Math.min(min , scc(next));
+            // 다음 정점이 방문이 안되었고 , 아직 강연결 집합에 포함되지 않았으면
+            else if(scc[next] == 0) min = Math.min(min , id[next]);
+        }
+
+
+        // 본인이 사이클의 최대 부모이면 stack 에서 뺌
+        if(id[a] == min){
+            sccSize++;
+            while(true){
+                int b = stack.pop();
+                scc[b] = sccSize; // b 를 scc에 소속시킴
+                if(a == b) break;
+            }
+        }
+
+        // 이전 정점들에게 본인의 최대 부모를 알려주기 위해서
+        return min;
+    }
+
+    // 위상 정렬
+//    public static void stomachSort(int a){
+//        for(Integer next : sccGraph.get(a)){
+//            entry[next]++;
+//            if(entry[next] != 2) // 아직 방문하기 직전의 next 만 진행
+//                stomachSort(next);
+//        }
+//    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        String[] in = input.readLine().split(" ");
+        V = Integer.parseInt(in[0]);
+        E = Integer.parseInt(in[1]);
+
+        id = new int[V + 1];
+        scc = new int[V + 1];
+        vertex = new int[V + 1];
+        resto = new boolean[V + 1]; // 레스토랑인지 아닌지 판단
+
+        for(int i = 0; i <= V; i++){
+            graph.add(new ArrayList<>());
+        } // graph 초기화
+
+        for(int i = 0; i < E; i++){
+            int[] edge = Arrays.stream(input.readLine().split(" "))
+                            .mapToInt(Integer::parseInt).toArray();
+
+            graph.get(edge[0]).add(edge[1]); // a -> b
+
+        } // 입력 받기
+
+        for(int i = 1; i <= V; i++){
+            vertex[i] = Integer.parseInt(input.readLine());
+        }
+
+        S = Integer.parseInt(input.readLine().split(" ")[0]);
+        resto = new boolean[V+1];
+
+        Arrays.stream(input.readLine().split(" "))
+                .mapToInt(Integer::parseInt).forEach(e->resto[e]=true);
+
+        for(int i = 1; i <= V; i++){
+            // 아직 방문안했으면
+            if(id[i] == 0) scc(i);
+        } // scc 집합 만듦
+
+        // scc 집합 만들었으니까 , 위상정렬을 진행하면 됨
+        // 일단 sccSize 에 따라서 graph 도 만들고 해야함
+
+        sccVertex = new int[sccSize + 1];
+        dp = new int[sccSize + 1];
+        sccResto = new boolean[sccSize + 1];
+
+        for(int i = 0;i <= sccSize; i++){
+            sccGraph.add(new ArrayList<>());
+        }
+
+        for(int i = 1; i <= V; i++){ // 모든 정점을 돌아 다니며 , SCC의 위상정렬을 정리할 것임
+            // sccVertex 에 해당 집합의 가중치 값을 저장
+            sccVertex[scc[i]] += vertex[i];
+
+            // scc에 지금 현재까지는 resto 가 있다라는 증거가 없을 때에만
+            if(!sccResto[scc[i]])
+                // 그러고서 현재 집합에 resto 가 포함이 되어 있나 안되어 있나를 집어넣고 , true 로 결정되면 이집합에 대해서는 다시 진행하지 않음
+                sccResto[scc[i]] = resto[i];
+            for(Integer next : graph.get(i)){
+                // 같은 집합에 포함된 , SCC 면 안됨
+                if(scc[next] != scc[i]){
+
+                    // 이제 여기에서 발견하는 정점을 entry 를 추가하는 것과 동시에 graph 에 추가하자.
+                    // SCC 사이에 간선이 두개 이상이 생길 수 있는데 , 짜피 결과값에는 영향을 주지 않을 것 같으니 , 넘어가자.
+
+                    // sccGraph 에다가 집합과 집합의 간선을 생성해줌
+                    sccGraph.get(scc[i]).add(scc[next]);
+                } // graph 형성 해주고
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        // 진입차선이 0 부터 시작하는 놈이 아니라 , 시작정점이 주어졌었음
+//        stomachSort(scc[S]);
+        queue.add(scc[S]);
+        dp[scc[S]] = sccVertex[scc[S]];
+        if(sccResto[scc[S]]) res = dp[scc[S]];
+
+
+        while(!queue.isEmpty()){
+            int a = queue.poll();
+
+            for(Integer next : sccGraph.get(a)){
+                if(dp[next] < dp[a] + sccVertex[next]){
+                    dp[next] = dp[a] + sccVertex[next];
+                    queue.add(next);
+
+                    if(sccResto[next]) res = Math.max(res , dp[next]);
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(scc));
+//        System.out.println(Arrays.toString(entry));
+//        System.out.println(Arrays.toString(sccVertex));
+//        System.out.println(Arrays.toString(sccResto));
+
+//        while (!queue.isEmpty()) {
+//
+//            // 해당 SCC 집합을 받아냄
+//            Integer a = queue.poll();
+//
+//            // dp 에다가 값을 더해줄 것임 , 그래야지 이전 진입차선들이 정해놓은 값과 , 본인의 값을 더할 수 있음
+//            dp[a] += sccVertex[a];
+//
+//            if(sccResto[a]) res = Math.max(res , dp[a]);
+//
+//            for(Integer next : sccGraph.get(a)){
+//                // next 를 실행
+//                if(--entry[next] == 0) // 0이 되면 집어넣어주고
+//                    queue.add(next);
+//
+//                // 현재 본인의 값이 dp[next] 보다 크면 넣어줌
+//                if(dp[next] < dp[a])
+//                    dp[next] = dp[a];
+//            }
+//        }
+
+        System.out.println(res);
     }
 }
--- ideaCodeTest/Test/src/Main.java
+++ ideaCodeTest/Test/src/Main.java
@@ -1,141 +1,67 @@
-import java.util.Random;
-
-/*
-다음과 같은 스터디룸의 예약 신청 정보를 관리하는 프로그램을 작성하세요.
-스터디룸은 1시에 문을 열고 6시에 문을 닫으며, 스터디룸이 문을 연 시간 내에 1시간 단위로 예약 가능하다.
-각 예약은 예약 번호, 시작시간, 종료시간으로 이루어지며, 이 세가지 정보는 모두 정수형이다.
-
-- 입력: 없음
-- 출력: 전체 예약 정보, 예약 길이별 예약 건수
-
-1. 메인 클래스
-main 메소드에서  다음 작업을 차례대로 수행
-(1) 크기가 n인 예약 배열을 생성 (단, n = 10)
-(2) 예약 번호는 1, 2, 3..., 시작시간, 종료시간은 랜덤 값인 예약 객체를 n개 생성하여 배열에 저장
-    이 스터디룸 정책에 맞도록 랜덤 값을 생성해야 함
-    예약 확정이 아니라 예약 신청을 받는 것이므로 시간이 중복되어도 된다.
-    힌트: 스터디룸 정책에 따르면 시작시간은 1~5이며, 종료시간은 시작시간+1~6이다.
-(3) n개의 예약 각각에 대해 시작시간, 종료시간, 길이를 출력
-(4) 각 길이의 예약이 몇개씩인지 구함 (중첩 반복문을 사용하지 말고 수행시간 복잡도가 O(n)이 되도록 작성해 볼 것)
-    힌트: 각 길이(1~5)의 예약 갯수를 세기 위해 크기가 5인 count 배열을 이용
-(5) 각 길이의 예약 수를 출력
-
-2. Reservation 클래스 ********* lab1_2와 동일
-정수형 예약 번호, 시작시간, 종료시간으로 구성된 예약을 표현하는 클래스
-private 인스턴스 변수 :
-       예약 번호(id), 시작시간(startTime), 종료시간(endTime)
-       이 필드값의 타당성은 고려하지 말 것. 즉, 아무 정수값이라도 저장되도록 할 것
-public 메소드 :
-       생성자, getId, getStartTime, getEndTime를 정의하고
-       길이를 조회하는 메소드, toString 오버라이드 등, 그밖의 메소드들은 각자 알아서 정의
-
-
-- 실행 예:
-hw1_2 : 홍길동
-1 1시~3시 2시간
-2 4시~6시 2시간
-3 4시~5시 1시간
-4 1시~6시 5시간
-5 5시~6시 1시간
-6 5시~6시 1시간
-7 2시~6시 4시간
-8 5시~6시 1시간
-9 3시~6시 3시간
-10 2시~6시 4시간
-1시간 = 4
-2시간 = 2
-3시간 = 1
-4시간 = 2
-5시간 = 1
-
------------------------------------
-목적
-- 알고리즘 과제 수행에 앞서 객체 배열 사용을 연습한다.
-- 랜덤 넘버 생성을 연습한다.
-
------------------------------------
-유의사항
-- 적절한 comment
-- 들여쓰기 철저히 할 것
-- 식별자 이름을 자바 관례에 맞게 적절히 붙일 것
-- 프로그램 맨 앞에 과제코드와 본인의 이름을 출력할 것
-- 입출력 형식을 실행 예와 동일하게 할 것
-
------------------------------------
-제출
-- 소스코드파일(.java)
-- 실행가능한 jar 파일(과제코드.jar)
-- LMS 과제 제출시 설명란에 이번 과제 수행에 관해 다음 항목을 적으세요. (1점) -- 프로그램을 제출해야 1점
-     (1) 소요 시간
-     (2) 도움 받은 정도
-     (3) 어려운 점
-     (4) 느낀 점
- */
-
-import java.util.Random;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hw4_1 : 김자경");
-        Random random = new Random();
-        int n = 10;
+    public static void main(String[] args) throws Exception{
 
-        //(1) 크기가 n인 예약 배열을 생성 (단, n = 10)
-        Reservation[] array = new Reservation[n];
+        // 파일 생성 주기
+        int diff = 3000;
 
-        //(2) 예약 번호는 1, 2, 3..., 시작시간, 종료시간은 랜덤 값인 예약 객체를 n개 생성하여 배열에 저장
-        for (int i = 0; i < array.length; i++) {
-            int startTime = random.nextInt(5) + 1; //1~5
-            int endTime = startTime + 1 + random.nextInt(6 - startTime); //2~6, 시작시간보다 최소 1 커야 함
-            array[i] = new Reservation(i + 1, startTime, endTime);
-        }
+        // index == targetIndex 이면 실행
+        int targetIndex = diff / 100; // targetIndex is 700
 
-        //(3) n개의 예약 각각에 대해 시작시간, 종료시간, 길이를 출력
-        for (int i = 0; i < array.length; i++)
-            System.out.println(array[i].toString());
+        int index = targetIndex;
 
-        //(4) 예약 길이를 기준으로 오름차순 정렬
-        countingSort(array);
+        // 이전 시각과 현재 시각이 비교했을 때 , diff 의 차이가 있어야함
+        while (true) {
 
-        //(5) 정렬된 n개의 예약 각각에 대해 시작시간, 종료시간, 길이를 출력
-        System.out.println("길이 오름차순 정렬 결과 = ");
-        for (int i = 0; i < array.length; i++)
-            System.out.println(array[i].toString());
-    }
+            Date now = new Date();
+            String format = "yyyy-MM-dd HH.mm";
+            DateFormat df = new SimpleDateFormat(format);
+            String fileName = df.format(now);
 
-    //Reservation형 배열을 매개변수로 받아 예약 길이(1~5시간)를 기준으로 오름차순 정렬 - 시작 복잡도가 O(n)이어야 함
-    private static void countingSort(Reservation[] list) {
-        // 정렬 결과는 int 가 아닌 Reservation 으로 받아야 합니다.
-//        int[] result = new int[list.length]; // 정렬 결과
-        Reservation[] result = new Reservation[list.length];
+            // 폴더 새로 생성
+            File dir = new File("C:/Temp/" + fileName);
+            if (dir.exists() == false) {
+                dir.mkdirs();
+            }
 
-        // 1 부터 k 까지의 자연수는 1 ~ 5 이다 , 그렇기 때문에 5 크기의 배열을 선언하면 됩니다.
-        int[] cnt = new int[5]; // 1부터 k까지의 자연수가 각각 몇 번씩 나타나는지 count
+            // targetIndex 보다 index 가 작으면 실행이 된다 , targetIndex == index 인 순간에 실행이 되는 것이다.
+            if(index < targetIndex) Thread.sleep(100);
 
-        // 0으로 초기화 , 초기화 안해도 됨 , 이미 선언하자말자 초기화 되어 있음
-//        for (int i = 0; i <= list.length; i++) {
-//            cnt[i] = 0;
-//        }
+            // 1. 폴더 만들기
+            // 날짜로 폴더이름
+            if(targetIndex != index++) continue;
 
-        // 값이 i인 원소의 총 수
-        for (int i = 0; i < list.length; i++) {
-            cnt[list[i].getLength() - 1]++;
-        }
+            index = 0;
+            System.out.println(LocalDateTime.now().getSecond());
 
-        // i보다 작거나 같은 원소의 총 수 -> 누적 합 계산
-        for (int i = 1; i < cnt.length; i++) {
-            cnt[i] = cnt[i] + cnt[i - 1];
-        }
+            // 2. 파일 복사
+            // 파일 이름 랜덤으로 바꿔줌.
+            UUID uuid = UUID.randomUUID();
 
-        // 누적 합을 이용해서 최종적으로 정렬
-        for (int i = list.length - 1; i != -1; i--) {
-            result[cnt[list[i].getLength() - 1] - 1] = list[i]; // result의 cnt[list[i]]인덱스에 list[i]값 저장
-            cnt[list[i].getLength() - 1]--;
-        }
+            String sourceFileName = "C:/Temp/source.zip";
+            String targetFileName = "C:/Temp/" + fileName + "/" + uuid + ".zip";
 
-        // result 를 array 에 카피 (list 가 array 의 주소를 받았음 , call by reference)
-        for (int i = 0; i < result.length; i++){
-            list[i] = result[i];
-        }
+            FileInputStream fis = new FileInputStream(sourceFileName);
+            FileOutputStream fos = new FileOutputStream(targetFileName);
+
+            int readByteNo;
+
+            byte[] readBytes = new byte[40];
+
+            while ((readByteNo = fis.read(readBytes)) != -1) {
+                fos.write(readBytes, 0, readByteNo);
+
+            } // while
+
+            fos.flush();
+            fos.close();
+            fis.close();
+
+        } // while
     }
 }
