060659c8f38ba28b22ac96d6630eed7a3c900ec6
jaeyeon kim <jakind@naver.com>
2022-04-29 07:56:11 +0900

    programmers directory 추가

--- BJ/Java/1701_problem/src/Main.java
+++ BJ/Java/1701_problem/src/Main.java
@@ -1,17 +1,2 @@
-import java.util.*;
-import java.io.*;
-import java.util.function.Function;
-
-// 1701 : Cubeditor
-
-/*
--- 전제조건
-
--- 틀설계
- */
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Function<String, Integer> fun = Integer::parseInt;
-    }
+package PACKAGE_NAME;public class Main {
 }
