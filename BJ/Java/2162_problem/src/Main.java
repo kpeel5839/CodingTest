308acc290f3b718b12cc96c7351ffbaa551d7eff
jaeyeon kim <jakind@naver.com>
2022-04-29 07:56:31 +0900

    programmers directory 추가

--- BJ/Java/2162_problem/src/Main.java
+++ BJ/Java/2162_problem/src/Main.java
@@ -1,2 +1,208 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 2162 : 선분 그룹
+
+/**
+ * -- 전제 조건
+ * 여러개의 선분들이 주어졌을 때, 교차하는 선분들의 그룹의 개수를 출력하고,
+ * 그 그룹 중에서, 선분이 많은 그룹의 선분 개수를 출력하라.
+ *
+ * -- 틀 설계
+ * 기하학으로 해결하는 문제이고, 거기에다가 union find 까지 써야하는 문제이다.
+ *
+ * notion 에다가 정리해놓았다.
+ * https://www.notion.so/2162-ed1e5e1fca20407dad8317c40daec746
+ */
+public class Main {
+    public static int[] parent; // parent 저장할 변수
+    public static int[] count; // parent 를 통해서, 어떠한 인덱스의 포인터가, 어떠한 그룹에 속해있고, 그 그룹에는 몇개의 선분이 있는지 확인하기 위한 변수
+    public static final int INF = 5000; // 이 문제에서의 x, y 최대 값, 즉 유효하지 않은 숫자.
+
+    public static class Point {
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+
+        public Point(double x1, double y1, double x2, double y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        } // 생성자 정의
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) {
+           return a;
+        }
+
+        return parent[a] = find(parent[a]); // 최종적으로 반환된, 부모 노드를 본인의 부모노드로 등록
+    }
+
+    public static void union(int a, int b) {
+        parent[b] = a; // b 집합의 부모로 a 를 집어넣는다.
+    }
+
+    // 교점 x를 구하는 method
+    public static double getX(Point p1, Point p2) {
+        double x1 = p1.x1;
+        double y1 = p1.y1;
+        double x2 = p1.x2;
+        double y2 = p1.y2;
+
+        double x3 = p2.x1;
+        double y3 = p2.y1;
+        double x4 = p2.x2;
+        double y4 = p2.y2; // x1, x2, x3, x4, y1, y2, y3, y4 즉 두 선분의 포인터들을 다 뽑아냄, (가독성 좋게
+
+        double deno = ((x1 - x2) * (y3 - y4)) - ((x3 - x4) * (y1 - y2)); // 분모, (x1 - x2)(y3 - y4) - (x3 - x4)(y1 - y2)
+        double mole = (((x1 * y2) - (x2 * y1)) * (x3 - x4)) - (((x3 * y4) - (x4 * y3)) * (x1 - x2)); // 분자, (x1y2 - x2y1)(x3 - x4) - (x3y4 - x4y3)(x1 - x2)
+
+        if (deno == 0) {
+            return INF + 1;
+        }
+
+        return mole / deno;
+    }
+
+    // 교점 Y를 구하는 method
+    public static double getY(Point p1, Point p2) {
+        double x1 = p1.x1;
+        double y1 = p1.y1;
+        double x2 = p1.x2;
+        double y2 = p1.y2;
+
+        double x3 = p2.x1;
+        double y3 = p2.y1;
+        double x4 = p2.x2;
+        double y4 = p2.y2; // x1, x2, x3, x4, y1, y2, y3, y4 즉 두 선분의 포인터들을 다 뽑아냄, (가독성 좋게
+
+        double deno = ((x1 - x2) * (y3 - y4)) - ((x3 - x4) * (y1 - y2)); // 분모, (x1 - x2)(y3 - y4) - (x3 - x4)(y1 - y2)
+        double mole = (((x1 * y2) - (x2 * y1)) * (y3 - y4)) - (((x3 * y4) - (x4 * y3)) * (y1 - y2)); // 분자, (x1y2 - x2y1)(y3 - y4) - (x3y4 - x4y3)(y1 - y2)
+
+        if (deno == 0) {
+            return INF + 1;
+        }
+
+        return mole / deno;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Double> fun = Double::parseDouble;
+
+        int N = Integer.parseInt(br.readLine());
+        parent = new int[N];
+        count = new int[N];
+        Point[] point = new Point[N];
+        String[] input;
+
+        for (int i = 0; i < N; i++) {
+            parent[i] = i; // 본인으로 채워줌
+        }
+
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+
+            double x1 = fun.apply(input[0]);
+            double y1 = fun.apply(input[1]);
+            double x2 = fun.apply(input[2]);
+            double y2 = fun.apply(input[3]);
+
+            point[i] = new Point(x1, y1, x2, y2);
+        }
+
+        for (int i = 0; i < N; i++) { // 비교하는 것이니까, 중복되지 않도록 이런식으로 포문을 구성해줌
+            for (int j = i + 1; j < N; j++) {
+                Point p1 = point[i];
+                Point p2 = point[j];
+
+                double res = getX(p1, p2); // 두 직선의 교점을 얻음
+
+                if (res == INF + 1) { // 두 직선이 평행하거나 일치할 때
+                    // x = a 인 경우
+                    if ((p1.x2 - p1.x1) == 0 && (p2.x2 - p2.x1 == 0)) {
+                        // 이제 평행하는지 안하는 지를 보면 된다.
+                        if (p1.x1 != p2.x1) { // 평행한 경우
+                            continue;
+                        }
+
+                        if ((p1.y1 < Math.min(p2.y1, p2.y2) && p1.y2 < Math.min(p2.y1, p2.y2))
+                                || (p2.y1 < Math.min(p1.y1, p1.y2) && p2.y2 < Math.min(p1.y1, p1.y2))) { // 두 선분이 교차하지 않는 경우, 솔직히 이것보다 더 깔끔하게 할 수 있을 것 같은데, 틀리면 괜히 시간낭비 오질 것 같아서 안함
+                            continue;
+                        }
+
+                        union(find(i), find(j));
+                        continue;
+                    }
+
+                    // x == a 가 아닌 경우, 이제 y == a 도 고려해주긴 해야함
+
+                    // 이제, m 과, n 을 구하자.
+                    // m = 기울기, n = y절편
+
+                    // 기울기는, (y2 - y1) / (x2 - x1)
+                    double m1 = (p1.y2 - p1.y1) / (p1.x2 - p1.x1);
+                    double m2 = (p2.y2 - p2.y1) / (p2.x2 - p2.x1);
+
+                    // n은 이런식으로 유도 가능하다, 보통의 일반함수는 mx + n 형태로 이루어져 있으니까 m(x - x1) - y1 에서 mx - mx1 - y1 이니까, n 은 - mx + y
+                    double n1 = (-m1 * p1.x1) + p1.y1;
+                    double n2 = (-m2 * p2.x1) + p2.y1;
+
+                    if (n1 != n2) { // 둘이 일치하지 않는 경우, 일단 평행한 것은 확실하니까 기울기는 같음 res = INF + 1 이였기 때문에, 즉 평행하다.
+                        continue;
+                    }
+
+                    // 두 직선이 일치하여도 만나지 않는 경우
+                    if ((p1.x1 < Math.min(p2.x1, p2.x2) && p1.x2 < Math.min(p2.x1, p2.x2))
+                            || (p2.x1 < Math.min(p1.x1, p1.x2) && p2.x2 < Math.min(p1.x1, p1.x2))) {
+                        continue;
+                    }
+
+                    // y = a 인 경우도 고려
+                    if ((p1.y1 < Math.min(p2.y1, p2.y2) && p1.y2 < Math.min(p2.y1, p2.y2))
+                            || (p2.y1 < Math.min(p1.y1, p1.y2) && p2.y2 < Math.min(p1.y1, p1.y2))) {
+                        continue;
+                    }
+
+                    union(find(i), find(j));
+                } else { // 두 직선이 평행하거나, 일치하지 않고 교점이 존재할 때
+                    // 일단 먼저 x 좌표가, 포함이 되는 지 확인 해야함, 그 다음에 y 좌표까지 확인할 것임
+                    if (res >= Math.min(p1.x1, p1.x2) && res <= Math.max(p1.x1, p1.x2)
+                            && res >= Math.min(p2.x1, p2.x2) && res <= Math.max(p2.x1, p2.x2)) {
+
+                        res = getY(p1, p2); // 일단, x 좌표가 포함 되는지 확인을 했으니까, 다시 res 를 구해서, 교점 Y가 다시 포함되는지 확인해야 함
+
+                        if (-INF <= res && res <= INF) { // 좌표가, 범위에 포함되는 지
+                            if (res >= Math.min(p1.y1, p1.y2) && res <= Math.max(p1.y1, p1.y2)
+                                    && res >= Math.min(p2.y1, p2.y2) && res <= Math.max(p2.y1, p2.y2)) {
+                                union(find(i), find(j)); // y 좌표까지 포함이 되면, 또 union
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // 이제 다 진행했으면, find, union 을 한 것을 가지고, count 값을 계산해주어야 한다.
+        for (int i = 0; i < N; i++) {
+            count[find(i)]++;
+        } // i 들의 부모를 최종적으로 검색하여서, 그 그룹의 개수들을 구함
+
+        int groupCount = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < N; i++) {
+            // count 가 0 이 아닌 애들의 숫자가 group 수임, 왜냐하면, 최종 부모노드를 찾아서 count를 갱신하였기 때문에
+            if (count[i] != 0) {
+                groupCount++;
+                maxCount = Math.max(count[i], maxCount);
+            }
+        }
+
+        System.out.println(groupCount + "\n" + maxCount);
+    }
 }
--- BJ/Python/14888_problem.py
+++ BJ/Python/14888_problem.py
@@ -1,64 +1,75 @@
-#안되는 경우를 생각해보자 
-#일단 my_team 이라고 가정하였을 때 같은 선수가 또 들어가서는 안됨.. 선수 리스트를 만들까? [1,2,3,4] 이런식으로 사이즈가 4이면 0,1,2,3이 있짢아
-#잘했다 재연아 이렇게 풀기만 하면 되는 거야 몇 시간 걸리는 거 상관 없어
-min = 1000000001
-size = int(input())
-team_list = [list(map(int,input().split())) for _ in range(size)]
-friend_team = []
-#my_team을 1차원 리스트로 만들자
-def dfs(my_team):
-    global min
-    global friend_team
+from multiprocessing.dummy import Value
+import sys
 
-    if len(my_team) > 1 and my_team[len(my_team) - 1] < my_team[len(my_team) - 2]: #오름 차순이 아니라면 재귀 끝냄
-            return
+'''
+-- 틀 설계
+caculate() 함수를 선언 numberList , sequence를 주면 계산
+dfs() sequence를 만드는 과정
+'''
 
-    if len(my_team) == size / 2: #my team 의 사이즈가 사이즈 / 2 라면 끝난 것임
-        my_powers = my_power(my_team) #각각의 점수들 계산
-        enemy_powers = enemy_power(my_team) 
-        if min > abs(enemy_powers - my_powers): #그래서 차이를 계산하는 것
-            min = abs(enemy_powers - my_powers)
-            friend_team = my_team.copy()
-            
-    else: 
-        for i in range(size):
-            if i not in my_team: #내 마이팀 리스트에 해당 선수가 없으면 그냥 들어가도 됨
-                my_team.append(i) #이게 재귀가 끝나서 다시 돌아왔을 떄 영향을 받지 않도록만 하면 됨
-                dfs(my_team) #지금 내팀 넘어감
-                my_team.pop() #이거 이제 dfs 끝나면 다시 처음부터 시작해야하니깐 pop 해주고 다시 처음부터 넣음
+n = int(input())
+sequence = [0 for _ in range(n - 1)]
 
-def my_power(my_team): #우리팀 스코어 반환
-    tmp_sum = 0
-    for i in range(size):
-        for j in range(size):
-            if i == j: #i == j 는 그냥 지나감
-                continue
-            if i not in my_team: # i 선수가 우리팀에 없으면 지나감
-                continue
-            if j not in my_team: # j 선수가 우리팀에 없으면 지나감
-                continue
-            tmp_sum += team_list[i][j] #우리팀 애들만 다 더해줌 짜피 j i 각각 똑같은 값 한번 씩 돌기 때문에 한번만 써준 것
-    return tmp_sum
-def enemy_power(enemy_team):
-    tmp_sum = 0
-    for i in range(size): #이것은 그냥 위에서의 반대
-        for j in range(size):
-            if i == j:
-                continue
-            if i in enemy_team:
-                continue
-            if j in enemy_team:
-                continue
-            tmp_sum += team_list[i][j]
-    return tmp_sum
+# def calculate():
+#     global sequence , numberList , max , min
+#     '''
+#     리스트의 첫번째 값을 value에다가 넣고
+#     sequence 에 있는 0 , 1 , 2 , 3 대로 연산을 하는데
+#     0 = 더하기 , 1 = 뺄셈 , 2 = 곱셉 , 3 = 나눗셈이다
+#     '''
+#     value = numberList[0]
+#     for i in range(n - 1):
+#         if sequence[i] == 0:
+#             value += numberList[i + 1];
+#         elif sequence[i] == 1:
+#             value -= numberList[i + 1];    
+#         elif sequence[i] == 2:
+#             value *= numberList[i + 1];
+#         else:
+#             value = int(value / numberList[i + 1]);
+        
+#     if value < min:
+#         min = value
+#     if value > max:
+#         max = value
 
-dfs([]) #우리팀이 없는 상태에서 넘기기
-print(min) #가장 작은 값을 출력 여기까지가 정답임 이 다음부터는 그냥 최고의 팀을 뽑은 것임 그냥 내 맘
-enemy_team = []
-for i in range(size):
-    if i not in friend_team:
-        enemy_team.append(i + 1)
-for i in range(len(friend_team)):
-    friend_team[i] += 1
-print(friend_team)
-print(enemy_team)
+def dfs(depth, score):
+    '''
+    score들을 그때 그때 맞는 연산자로 계산해가면서 depth == n - 1까지 진행한다.
+    연산 순서를 나타내는 sequence와 해당 sequence에 들어가야 할 연산자들의 개수들을 정리한
+    oper 배열을 global로 선언한다.
+    depth 가 sequence의 크기인 n - 1까지 도달하면 재귀호출을 끝낸다.
+    0 , 1 , 2 , 3 각각 덧셈 , 빼기 , 곱셈 , 나눗셈 중 하나를 사용하면 oper 배열에서 하나를 빼주고
+    재귀호출을 진행해준다음에 재귀 호출이 끝나게 되면 다시 oper[i] -= 1을 해주었던 것을 oper[i] += 1 해줌으로써 원래의 상태로 되돌려놓는다.
+    그러고서 depth == n - 1이 될 때 마다 min , max 를 비교해서 값을 변경해주면 종료
+    '''
+    global oper , sequence , n , min , max , numberList
+    if depth == n - 1:    
+        if min > score:
+            min = score
+        if max < score:
+            max = score
+        return
+    for i in range(4):
+        if oper[i] != 0:
+            oper[i] -= 1
+            if(i == 0):
+                dfs(depth + 1 , score + numberList[depth + 1])
+            elif(i == 1):
+                dfs(depth + 1 , score - numberList[depth + 1])
+            elif(i == 2):
+                dfs(depth + 1 , score * numberList[depth + 1])
+            else:        
+                dfs(depth + 1 , int(score / numberList[depth + 1]))
+            oper[i] += 1
+
+numberList = list(map(int , input().split()))
+oper = list(map(int , input().split()))
+max = -2100000000
+min = 2100000000
+
+
+
+dfs(0 , numberList[0])
+
+print(str(max) + "\n" + str(min))
