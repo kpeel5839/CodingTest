5bef8ba9f2b68c2ddf12e64cd5b4ed01815b741f
jaeyeon kim <jakind@naver.com>
2022-05-22 00:38:47 +0900

    문제 추가'

---
+++ BJ/Java/3678_problem/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 3678 : 카탄의 개척자
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
---
+++ BJ/Java/5014_problem/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 5014 : 스타트링크
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
