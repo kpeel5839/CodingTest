8873105f9feda56e956de064df0c3e19d9197bad
jaeyeon kim <jakind@naver.com>
2022-05-23 11:23:43 +0900

    5337번 문제 해결

--- BJ/Java/17478_problem/src/Main.java
+++ BJ/Java/17478_problem/src/Main.java
@@ -40,7 +40,7 @@ public class Main {
             sb.append("____");
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
