2e3ca8abaf7d0e489d418f20be7fff275b214860
jaeyeon kim <jakind@naver.com>
2022-06-06 09:40:15 +0900

    10699번 문제 해결

---
+++ BJ/Java/10699_problem/src/Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
