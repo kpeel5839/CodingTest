b2147cc871205e01b82f0b1e668d34e3125d16da
jaeyeon kim <jakind@naver.com>
2022-06-17 00:49:07 +0900

    2446번 문제 해결

---
+++ BJ/Java/2446_problem/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int index = N;
+        boolean crease = false;
+
+        if (N == 1) {
+            sb.append("*");
+        } else {
+            while (true) {
+                for (int i = 0; i < N - index; i++) {
+                    sb.append(" ");
+                }
+
+                for (int i = 0; i < index * 2 - 1; i++) {
+                    sb.append("*");
+                }
+
+                if (crease) {
+                    index++;
+                } else {
+                    index--;
+                }
+
+                if (index == 1) {
+                    crease = true;
+                }
+
+                if (index == N + 1) {
+                    break;
+                }
+
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
