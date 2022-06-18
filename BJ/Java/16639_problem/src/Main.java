3886b80d6df90ecee40164ae20fa943573f3d38c
jaeyeon kim <jakind@naver.com>
2022-06-18 11:22:58 +0900

    문제 추가

---
+++ BJ/Java/16639_problem/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 16639 : 괄호 추가하기 3
+
+/**
+ * -- 전제조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
