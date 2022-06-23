5e8b1fab7c23378fd1d411a6ada747f6c0d0d44e
jaeyeon kim <jakind@naver.com>
2022-06-23 10:46:06 +0900

    문제 추가g

---
+++ BJ/Java/_5852_problem/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 5852 : Island Travels
+
+/**
+ * -- 전제조건
+ *
+ * -- 틀 설계
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
--- BJ/Java/_7420_problem/src/Main.java
+++ BJ/Java/_7420_problem/src/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 import java.io.*;
 import java.util.function.Function;
 
-// 18500 : 미네랄 2
+// 7420 : 맹독 방벽
 
 /**
  * -- 전제조건
  *
  * -- 틀 설계
  */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
