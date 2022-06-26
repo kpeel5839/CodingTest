a81509680eb7f8b3db3689e1be95d080a35a219b
jaeyeon kim <jakind@naver.com>
2022-06-26 18:12:09 +0900

    10991번 문제 해결

---
+++ BJ/Java/_10991_problem/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (i + 1); j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
