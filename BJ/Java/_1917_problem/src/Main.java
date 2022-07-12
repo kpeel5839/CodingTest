e9e1ec6f238624569a9fe587e47b42f85fe0b09d
jaeyeon kim <jakind@naver.com>
2022-07-12 14:04:29 +0900

    문제 추가

--- BJ/Java/_1917_problem/src/Main.java
+++ BJ/Java/_1917_problem/src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 1917 : 정육면체 전개도
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
 }
--- BJ/Java/_8980_problem/src/Main.java
+++ BJ/Java/_8980_problem/src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 8980 : 택배
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
 }
