90d6b13d1903abdb561a688718e0e1eee6089867
jaeyeon kim <jakind@naver.com>
2022-07-24 18:16:12 +0900

    문제 추가

---
+++ BJ/Java/_1016_problem/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 1016 : 제곱 ㄴㄴ 수
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1016_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
---
+++ BJ/Java/_17370_problem/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.Function;
+
+// 17370 : 육각형 우리 속의 개미
+
+/**
+ * -- 전제 조건
+ *
+ * -- 틀 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_17370_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> fun = Integer::parseInt;
+    }
+}
