28d194960800f9ceacebfbe4e2188305ef473627
jaeyeon kim <jakind@naver.com>
2022-07-29 22:13:46 +0900

    외판원 순회 문제 해결

--- BJ/Java/_2098_problem/src/Main.java
+++ BJ/Java/_2098_problem/src/Main.java
@@ -12,9 +12,55 @@ import java.io.*;
  * 8 8 9 0
  */
 public class Main {
+    static int N;
+    static int[][] map;
+    static int[][] dp;
+    static int INF = 100_000_000;
+
+    static int dfs(int now, int bit) {
+        if (bit == (1 << N) - 1) {
+            if (map[now][0] == 0) {
+                return INF;
+            } else {
+                return map[now][0];
+            }
+        }
+
+        if (dp[now][bit] != -1) {
+            return dp[now][bit];
+        }
+
+        dp[now][bit] = INF;
+
+        for (int i = 1; i < N; i++) {
+            if ((bit & (1 << i)) == 0 && map[now][i] != 0) {
+                dp[now][bit] = Math.min(dp[now][bit], dfs(i, bit | (1 << i)) + map[now][i]);
+            }
+        }
+
+        return dp[now][bit];
+    }
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_11049_problem/src"));
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2098_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new int[N][1 << N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.print(dfs(0, 1));
     }
 }
