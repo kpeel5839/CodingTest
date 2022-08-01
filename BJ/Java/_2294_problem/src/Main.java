ecd34e1a1b077dcbdf9733be2cc59de49355cfcf
jaeyeon kim <jakind@naver.com>
2022-08-01 19:09:50 +0900

    문제 추가

---
+++ BJ/Java/_2294_problem/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+// 2294 : 동전 2
+
+/**
+ *
+ * 예제
+ *
+ * 3 15
+ * 1
+ * 5
+ * 12
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2294_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
