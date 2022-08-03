1a40585ef9221ddbabf71e092bb81a0272a45c2f
jaeyeon kim <jakind@naver.com>
2022-08-03 21:54:37 +0900

    문제 추가

---
+++ BJ/Java/_17071_problem/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+// 17071 : 숨바꼭질 5
+
+/**
+ * 예제
+ * 
+ * 5 17
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_17071_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
