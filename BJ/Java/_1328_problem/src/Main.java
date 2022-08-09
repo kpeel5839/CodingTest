661807b7b9dbd0a8482ca01998d65aa7230c63f8
jaeyeon kim <jakind@naver.com>
2022-08-09 11:24:10 +0900

    1328번 문제 해결

---
+++ BJ/Java/_1328_problem/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+// 1328 : 고층 빌딩
+
+/**
+ * 예제
+ * 3 2 2
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1328_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
