fd02412fcee3622584498adf8d17d5d93039f230
jaeyeon kim <jakind@naver.com>
2022-08-11 21:48:43 +0900

    4811 번 문제 해결

--- BJ/Java/_2240_problem/src/Main.java
+++ BJ/Java/_2240_problem/src/Main.java
@@ -14,9 +14,45 @@ import java.io.*;
  * 1
  */
 public class Main {
+    static int T;
+    static int W;
+    static int[] drop;
+    static int[][] dp;
+
+    static int dfs(int depth, int cur, int move) {
+        if (depth == (T - 1)) { // 끝에 도달한 경우
+            return 0;
+        }
+
+        if (dp[depth][move] != -1) {
+            return dp[depth][move];
+        }
+
+        if (move == W) {
+            dp[depth][move] = (cur == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, cur, move);
+        } else {
+            dp[depth][move] = Math.max((1 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 1, (1 == cur ? move : move + 1)),
+                    (2 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 2, (2 == cur ? move : move + 1)));
+        }
+
+        return dp[depth][move];
+    }
+
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2240_problem/src/Main.java"));
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2240_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        T = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        dp = new int[T][W + 1];
+        drop = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            Arrays.fill(dp[i], -1);
+            drop[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.print(dfs(0, 1, 0));
     }
 }
--- BJ/Java/_4811_problem/src/Main.java
+++ BJ/Java/_4811_problem/src/Main.java
@@ -17,6 +17,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_4811_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+
+            if (N == 0) {
+                break;
+            }
+
+            long[][] dp = new long[N + 1][N + 1];
+            dp[N][N - 1] = 1;
+
+            for (int k = (2 * N - 2); k != -1; k--) {
+                for (int i = 0; i <= N; i++) {
+                    if (k < i) {
+                        break;
+                    }
+                    for (int j = 0; j <= N; j++) {
+                        if (i < j) {
+                            break;
+                        }
+
+                        if (i + j == k) {
+                            if (i == N) {
+                                dp[i][j] = dp[i][j + 1];
+                            } else {
+                                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                            }
+                        }
+                    }
+                }
+            }
+
+            bw.write(dp[0][0] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
