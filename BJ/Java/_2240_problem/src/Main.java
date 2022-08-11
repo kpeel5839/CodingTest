b31858fb392b2f6955d3fc3b9a13a1e8ad2d06d5
jaeyeon kim <jakind@naver.com>
2022-08-11 22:48:20 +0900

    2240 번 문제 해결

--- BJ/Java/_2240_problem/src/Main.java
+++ BJ/Java/_2240_problem/src/Main.java
@@ -12,30 +12,36 @@ import java.io.*;
  * 2
  * 1
  * 1
+ *
+ * 해맸던 이유...
+ * 아무리 봐도 로직이 맞았음 정확했음 정말
+ * 근데 왜 자꾸 틀리나 했는데
+ *
+ * 알고보니까 처음에도 움직일 수 있음...........................
  */
 public class Main {
     static int T;
     static int W;
     static int[] drop;
-    static int[][] dp;
+    static int[][][] dp;
 
     static int dfs(int depth, int cur, int move) {
         if (depth == (T - 1)) { // 끝에 도달한 경우
             return 0;
         }
 
-        if (dp[depth][move] != -1) {
-            return dp[depth][move];
+        if (dp[cur][depth][move] != -1) {
+            return dp[cur][depth][move];
         }
 
-        if (move == W) {
-            dp[depth][move] = (cur == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, cur, move);
+        if (W == move) {
+            dp[cur][depth][move] = (cur == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, cur, move);
         } else {
-            dp[depth][move] = Math.max((1 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 1, (1 == cur ? move : move + 1)),
-                    (2 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 2, (2 == cur ? move : move + 1)));
+            dp[cur][depth][move] = Math.max((1 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 1, 1 == cur ? move : move + 1),
+                    (2 == drop[depth + 1] ? 1 : 0) + dfs(depth + 1, 2, 2 == cur ? move : move + 1));
         }
 
-        return dp[depth][move];
+        return dp[cur][depth][move];
     }
 
     public static void main(String[] args) throws IOException {
@@ -45,14 +51,16 @@ public class Main {
 
         T = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
-        dp = new int[T][W + 1];
+        dp = new int[3][T][W + 1];
         drop = new int[T];
 
         for (int i = 0; i < T; i++) {
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp[1][i], -1);
+            Arrays.fill(dp[2][i], -1);
             drop[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.print(dfs(0, 1, 0));
+        System.out.print(Math.max((1 == drop[0] ? 1 : 0) + dfs(0, 1, 0)
+                , (2 == drop[0] ? 1 : 0) + dfs(0, 2, 1)));
     }
 }
