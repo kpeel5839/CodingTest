1bfe7ba70101252682cd77d13c4e910c6ea44771
jaeyeon kim <jakind@naver.com>
2022-08-23 16:48:24 +0900

    2698번 문제 해결

--- BJ/Java/_2698_problem/src/Main.java
+++ BJ/Java/_2698_problem/src/Main.java
@@ -17,9 +17,60 @@ import java.io.*;
  * 100 90
  */
 public class Main {
+    static int N;
+    static int K;
+    static int[][][] dp;
+
+    static int dfs(int depth, int sum, int cur) {
+        if (depth == N - 1) {
+            if (sum == K) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (K < sum) {
+            return 0;
+        }
+
+        if (dp[depth][sum][cur] != -1) {
+            return dp[depth][sum][cur];
+        }
+
+        dp[depth][sum][cur] = 0;
+        dp[depth][sum][cur] += dfs(depth + 1, sum, 0);
+        dp[depth][sum][cur] += dfs(depth + 1, cur == 1 ? sum + 1 : sum, 1);
+
+        return dp[depth][sum][cur];
+    }
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2698_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            dp = new int[N][K + 1][2];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j <= K; j++) {
+                    dp[i][j][0] = -1;
+                    dp[i][j][1] = -1;
+                }
+            }
+
+            bw.write(dfs(0, 0, 0) + dfs(0, 0, 1) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
