508c513241270ffc8f82d1bc2749f34fd86b2dac
jaeyeon kim <jakind@naver.com>
2022-08-30 16:07:56 +0900

    1563 번 문제 해결

--- BJ/Java/_1563_problem/src/Main.java
+++ BJ/Java/_1563_problem/src/Main.java
@@ -8,9 +8,46 @@ import java.io.*;
  * 4
  */
 public class Main {
+    static int N;
+    static int[][][] dp;
+    static int MOD = 1_000_000;
+
+    static int dfs(int depth, int late, int abs) {
+        if (depth == N) {
+            return 1;
+        }
+
+        if (dp[depth][late][abs] != -1) {
+            return dp[depth][late][abs];
+        }
+
+        dp[depth][late][abs] = 0;
+
+        if (late < 1) {
+            dp[depth][late][abs] += dfs(depth + 1, late + 1, 0);
+        }
+
+        if (abs < 2) {
+            dp[depth][late][abs] += dfs(depth + 1, late, abs + 1);
+        }
+
+        dp[depth][late][abs] += dfs(depth + 1, late, 0);
+
+        return dp[depth][late][abs] % MOD;
+    }
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1563_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N][2][3];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        System.out.println(dfs(0, 0, 0));
     }
 }
