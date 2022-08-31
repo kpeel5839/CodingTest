41b73ce0556326356878f51057c1f8af184e4a4e
jaeyeon kim <jakind@naver.com>
2022-08-31 13:41:05 +0900

    문제 추가

---
+++ BJ/Java/_1126_problem/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+// 1126 : 같은 탑
+
+/**
+ * 예제
+ * 3
+ * 2 3 5
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1126_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
---
+++ BJ/Java/_1162_problem/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+// 1162 : 도로 포장
+
+/**
+ * 예제
+ * 4 4 1
+ * 1 2 10
+ * 2 4 10
+ * 1 3 1
+ * 3 4 100
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1162_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
