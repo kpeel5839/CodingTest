e2908a742d1200826d1c4db5e35b51bd5fd44130
jaeyeon kim <jakind@naver.com>
2022-09-05 16:23:47 +0900

    1947번 문제 해결

--- BJ/Java/_1947_problem/src/Main.java
+++ BJ/Java/_1947_problem/src/Main.java
@@ -11,6 +11,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1947_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int MOD = 1_000_000_000;
+        long[] dp = new long[N + 1];
+
+        dp[1] = 0;
+
+        if (1 < N) {
+            dp[2] = 1;
+        }
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = ((i - 1) * (dp[i - 1] + dp[i - 2])) % MOD;
+        }
+
+        System.out.println(dp[N]);
     }
 }
