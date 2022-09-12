7f7142e751da6fb61e81cff49b2d169ee2754f22
jaeyeon kim <jakind@naver.com>
2022-09-12 16:06:12 +0900

    문제 추가

---
+++ BJ/Java/_1028_problem/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+// 1028 : 다이아몬드 광산
+
+/**
+ * 예제
+ * 5 5
+ * 01100
+ * 01011
+ * 11111
+ * 01111
+ * 11111
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1028_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
