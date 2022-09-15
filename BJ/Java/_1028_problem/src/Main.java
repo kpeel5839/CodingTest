a9eeea024e1d20a5ffd17eeeb5de5d92f0832b13
jaeyeon kim <jakind@naver.com>
2022-09-15 16:33:05 +0900

    1028 번 틀린 풀이법

--- BJ/Java/_1028_problem/src/Main.java
+++ BJ/Java/_1028_problem/src/Main.java
@@ -13,9 +13,92 @@ import java.io.*;
  * 11111
  */
 public class Main {
+    static int R;
+    static int C;
+    static int ans = 0;
+    static int[][] left;
+    static int[][] right;
+    static int[][] map;
+
+    static boolean outOfRange(int y, int x) {
+        if (y < 0 || y >= R || x < 0 || x >= C) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1028_problem/src/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        left = new int[R][C];
+        right = new int[R][C];
+        map = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            String string = br.readLine();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = string.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 1) {
+                    int y = i - 1;
+                    int xLeft = j - 1;
+                    int xRight = j + 1;
+
+                    if (!outOfRange(y, xLeft) && map[y][xLeft] == 1) {
+                        left[i][j] = left[y][xLeft] + 1;
+                    }
+
+                    if (!outOfRange(y, xRight) && map[y][xRight] == 1) {
+                        right[i][j] = right[y][xRight] + 1;
+                    }
+                }
+            }
+        }
+
+//        for (int c = 0; c < 2; c++) {
+//            System.out.println(c == 0 ? "Left" : "right");
+//            for (int i = 0; i < R; i++) {
+//                for (int j = 0; j < C; j++) {
+//                    System.out.print((c == 0 ? left[i][j] : right[i][j]) + " ");
+//                }
+//                System.out.println();
+//            }
+//        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 1) {
+                    int min = Math.min(left[i][j], right[i][j]); // 최대 이 사이즈임
+                    boolean diamond = false; // 다이아몬드가 맞으면 true 로 남아있음
+
+                    if (min * 2 > i) {
+                        min = i / 2;
+                    }
+
+                    int y = i - min;
+                    int xLeft = j - min;
+                    int xRight = j + min;
+
+                    if (min <= right[y][xLeft] && min <= left[y][xRight]) {
+                        diamond = true;
+                    }
+
+                    if (diamond) {
+                        ans = Math.max(ans, min + 1);
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
 }
