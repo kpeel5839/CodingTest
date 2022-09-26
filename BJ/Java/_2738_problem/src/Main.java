5a6ca16718d64302d95e09ca3908b8fbb5033463
jaeyeon kim <jakind@naver.com>
2022-09-26 10:42:16 +0900

    2738번 문제 해결

---
+++ BJ/Java/_2738_problem/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+// 2738 : 행렬 덧셈
+
+/**
+ * 예제
+ * 3 3
+ * 1 1 1
+ * 2 2 2
+ * 0 1 0
+ * 3 3 3
+ * 4 4 4
+ * 5 5 100
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_2738_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
