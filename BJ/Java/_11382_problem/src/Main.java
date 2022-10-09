89dbafccab8ac63b133040d88de37ce0284eb2dc
jaeyeon kim <jakind@naver.com>
2022-10-10 08:42:44 +0900

    11382번 문제 해결

---
+++ BJ/Java/_11382_problem/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_11382_problem/src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+    }
+}
