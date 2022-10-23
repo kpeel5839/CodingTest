27ad648ade3c1ec15b2baaa25d87193f99a31710
jaeyeon kim <jakind@naver.com>
2022-10-23 22:27:32 +0900

    16430번 문제 해결

---
+++ BJ/Java/_16430_problem/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_16430_problem/src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(b - a + " " + b);
+    }
+}
