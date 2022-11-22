a3fe8a6c1c54c6751dd4c21c1fe062717ca759fe
jaeyeon kim <jakind@naver.com>
2022-11-22 11:15:43 +0900

    5220 번 문제 해결

--- BJ/Java/_5220_problem/src/Main.java
+++ BJ/Java/_5220_problem/src/Main.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.io.*;
+
+// 5220 : Error Detection
+
+/**
+ * example
+ * 4
+ * 34173 1
+ * 45 1
+ * 15 0
+ * 31 0
+ */
+public class Main {
+
+    static int countOneOfBinaryNumber(int decimalNumber) {
+        int count = 0;
+//        String binaryNumber = decimalNumber == 0 ? "0" : "";
+
+        while (decimalNumber != 0) {
+            if (decimalNumber % 2 == 1) {
+                count++;
+            }
+
+//            binaryNumber = (decimalNumber % 2) + binaryNumber;
+            decimalNumber = decimalNumber >> 1;
+        }
+
+//        System.out.println(binaryNumber);
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_5220_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int numberOfTestCase = Integer.parseInt(br.readLine());
+
+        for (int test = 0; test < numberOfTestCase; test++) {
+            st = new StringTokenizer(br.readLine());
+            int divided = Integer.parseInt(st.nextToken());
+            boolean even = Integer.parseInt(st.nextToken()) == 0;
+            int count = countOneOfBinaryNumber(divided);
+            String answer = "";
+
+            if (count % 2 == 0) {
+                if (even) {
+                    answer = "Valid";
+                } else {
+                    answer = "Corrupt";
+                }
+            } else {
+                if (even) {
+                    answer = "Corrupt";
+                } else {
+                    answer = "Valid";
+                }
+            }
+
+            bw.write(answer + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
