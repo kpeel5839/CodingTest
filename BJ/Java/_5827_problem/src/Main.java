215adec389d68a6db6891f10b383bf6cbd6ffcee
jaeyeon kim <jakind@naver.com>
2022-11-28 15:30:22 +0900

    5827 번 문제 추가

---
+++ BJ/Java/_5827_problem/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+// 5827 : What's Up With Gravity
+
+/**
+ * example
+ * 5 5
+ * #####
+ * #...#
+ * #...D
+ * #C...
+ * ##.##
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_5827_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+    }
+}
